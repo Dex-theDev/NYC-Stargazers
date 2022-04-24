25b4842b871e584008069f7b711942c1ea21aace
Jaime <jaime.joseph1991@gmail.com>
2022-04-24 17:18:10 -0400

    added more styling and responsiveness

--- js/main.js
+++ js/main.js
@@ -10,6 +10,9 @@ const todayDescription = document.querySelector('.description-text')
 const button = document.querySelector('button')
 const picOfDay = document.querySelector('.picOfDay')
 const dayDescription = document.querySelector('.description-text2')
+const title1 = document.querySelector('.title1')
+const title2 = document.querySelector('.title2')
+
 //Fetch data on page load
 fetch(url)
     .then(res => res.json())
@@ -18,12 +21,19 @@ fetch(url)
         todayImage.src = data.hdurl
         todayDescription.innerHTML = data.explanation
         document.querySelector('span').innerText = data.date
+        title1.innerHTML = data.title
     })
     .catch(err => {
         console.log(`Not found ${err}`)
     })
 //Fetch data on button click
 button.addEventListener('click', getDate)
+
+//Hide iframe on page load
+document.querySelector('iframe').style.display = 'none'
+
+//Hide img placeholder on page load
+picOfDay.style.display = 'none'
 
 function getDate(){
     
@@ -34,9 +44,19 @@ const dateUrl = `https://api.nasa.gov/planetary/apod?api_key=8d3KhFXcP9llhHhKjjc
         .then(res => res.json())
         .then(data => {
             console.log(data)
-            picOfDay.src = data.hdurl
-            dayDescription.innerHTML = data.explanation
+            if(data.media_type === "video"){
+                document.querySelector('iframe').style.display = 'block'
+                document.querySelector('iframe').src = data.url
+                picOfDay.style.display = 'none'
+            } else if(data.media_type ==='image'){
+                document.querySelector('iframe').style.display = 'none'
+                picOfDay.style.display = 'block'
+                picOfDay.src = data.hdurl
+            }
 
+            title2.innerHTML = data.title
+            dayDescription.innerHTML = data.explanation
+            
         })
         .catch(err=> {
             console.log(`Not Found ${err}`)
